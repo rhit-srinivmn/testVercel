16d9f0d31f0d60e1ba0429a883f81528a070e4d7
rhit-srinivmn <101361171+rhit-srinivmn@users.noreply.github.com>
2024-11-18 04:26:50 -0500

    Initial Commit

---
+++ sample-vercel-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from Vercel!</h1>
+      <p>This is a sample React app to test Vercel deployment.</p>
+    </div>
+  );
+}
+
+export default App;
